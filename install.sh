93027d8abf4031a98f27ff17af92e6ba1e994ea6
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-05-25 19:07:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ clear; echo -e "installing..."
 apt update && apt upgrade -y > /dev/nill
 apt install python -y > /dev/nill
 pip install requests > /dev/null
+pip install wget > /dev/null
 
 python consult.py
 
