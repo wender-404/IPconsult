7d2498f4229ee54b733d29957d77074fa9ff98f1
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-05-25 01:37:35 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# version: 0.1
+# author: Dreifus-404 <https://github.com/Dreifus-404/>
+# Este programa faz as instalações necessarias para o funcionamento do codigo
+
+clear; echo -e "installing..."
+apt update && apt upgrade -y > /dev/nill
+apt install python -y > /dev/nill
+pip install requests > /dev/null
+
+python consult.py
+
+
