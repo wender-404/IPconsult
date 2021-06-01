8218ccd6dc386b4c9646ecb239c316d44977d96d
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-06-01 05:07:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 # author: Dreifus-404 <aslapoha03@gmail.com>
 # Este programa faz as instalações necessarias para o funcionamento do codigo
 
-clear; echo -e "\e[32m installing packages wait a moment\e[0m..."
-
+echo -e "\e[32m installing packages wait a moment\e[0m..."
 apt-get install python -y > /dev/null
-pip install -r requirements.txt
 
-python consult.py
+python -m pip install --upgrade pip
+python -m pip install flake8 pytest
+if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
