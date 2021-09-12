324d90dfc38700270cc6350b012b979e4c3755cb
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-09-12 10:50:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-# version: 0.1
-# author: Dreifus-404 <aslapoha03@gmail.com>
-# Este programa faz as instalações necessarias para o funcionamento do codigo
+# Authors     : Dreifus-404 <github.com/Dreifus-404>  |  Sysb1n <github.com/sidneypepo>
+# Description : this code install necessary packages of program
 
-echo -e "\e[32m installing packages wait a moment\e[0m..."
-apt-get install python -y > /dev/null
+function installing_packages {
+    cd $PREFIX/bin
 
-python -m pip install --upgrade pip
-python -m pip install flake8 pytest
-if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+    if [ -x python ]; then
+        echo -e "Package : Python is \e[32mInstalled!\e[0m."
+
+    else
+        echo -e "Package : Python \e[40;31mnot installed\e[0m\a\a"; sleep 5
+        echo -e "installing package..."
+
+        apt-get install python -y > /dev/null
+        cd ~/IPconsult/
+        ./install.sh
+    fi
+}
+
+function installing_importations {
+    cd ~/IPconsult/
+
+    if [ -x requeriments.txt ]; then pip install -r requirements.txt; fi
+
+    if [ $? != 0 ]; then
+        clear
+        echo "erro, verify your connection!."
+
+    else
+        clear
+        echo "installed packages of importation!."
+    fi
+
+}
+
+installing_packages
+installing_importations
